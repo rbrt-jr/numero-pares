f0031f46657ad932bbc824cdd7fbd2fd2b69f7bc
Your Name <you@example.com>
2023-08-08 11:51:12 +0100

    reinitialized files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+List<int> numeros = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+for (int i = 0; i < numeros.Count; i++)
+{
+    Console.Write(numeros[i]+", ");
+}
+Console.WriteLine();
+foreach (int pares in numeros)
+{
+    if(pares%2==0)
+    {
+        Console.Write($"{pares}, ");
+    }
+}
